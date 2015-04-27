08ac4cd64a3807d49aa29e5613c701e5dec4b376
Iago Suárez <xoan.iago.suarez@udc.es>
2015-04-28 01:18:31 +0200

    Fixed #13 Seguimiento en pantalla completa

--- src/static/site/js/video-player.js
+++ src/static/site/js/video-player.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
 
     var pos = [0,0];
     var detected_objs ;
+    var video_fps;
+
+    //Utilizadas para redimensionar el vídeo en caso de que estemos en pantalla completa
+    var fullscreenOn = false;
+    var video_proportion = 1;
+    var canvas_left_padding = 0;
+    var canvas_top_padding = 0;
 
     function paintFrame(){
-
-        function get_video_fps(video){
-            var ext = video.currentSrc.split('.').pop();
-            var fps = $(video).children('source[src$="'  + ext + '"]').attr('fps');
-            return fps;
-        }
 
         function paint_rect(context, xc, yc, w, h){
             context.beginPath();
@@ -28,7 +29,7 @@ $(document).ready(function(){
         var video = document.getElementById('video-player');
         context.clearRect ( 0 , 0 , canvas.width, canvas.height );
 
-        var frame_number = Math.round(video.currentTime * get_video_fps(video))
+        var frame_number = Math.round(video.currentTime * video_fps)
 
         //Para cada elemento en la frame pintamos su recuadro
         var frame = $($(detected_objs).find('frame')[frame_number])
@@ -39,7 +40,8 @@ $(document).ready(function(){
             var xc = parseInt($(this).find('box').attr('xc'));
             var yc = parseInt($(this).find('box').attr('yc'));
 
-            paint_rect(context, xc, yc, w, h)
+            paint_rect(context, video_proportion * xc, video_proportion * yc,
+                video_proportion * w, video_proportion * h)
         });
     }
 
@@ -53,28 +55,75 @@ $(document).ready(function(){
     }
     load_xml_objects();
 
+    function get_video_fps(video){
+        var ext = video.currentSrc.split('.').pop();
+        var fps = $(video).children('source[src$="'  + ext + '"]').attr('fps');
+        return fps;
+    }
 
-    /* Video canvas adjust size and START */
-    var video = document.getElementById("video-player");
-    // Cuando el vídeo ha cargado su tamano establecemos el
-    // mismo tamaño para el canvas
-    video.onloadedmetadata = function(){
+    function adjustCanvas(video){
+
         $('.drawing-layer').offset($('#video-player').offset());
         $('.drawing-layer').attr('height', $('#video-player').height());
         $('.drawing-layer').attr('width', $('#video-player').width());
 
         $( window, '#video-player' ).resize(function() {
+            // If we are in full screen mode
+            if(fullscreenOn){
+                video_height = video.videoHeight;
+                video_width = video.videoWidth;
+                screen_height = $(video).height();
+                screen_width = $(video).width();
+                video_proportion = screen_height / video_height;
+
+                if((video_width/video_height) < (screen_width/screen_height)){
+                    // El video es más cuadrado que la pantalla
+                    // Por tanto se contara un offset a la izq de la pantalla
+
+                    canvas_left_padding = (screen_width - (video_proportion*video_width))/2;
+                    canvas_top_padding = 0;
+                    $('.drawing-layer').attr('height', screen_height);
+                    $('.drawing-layer').attr('width', video_proportion*video_width);
+                } else {
+                    // El video es menos cuadrado que la pantalla
+                    // Por tanto se contara un offset a la parte superior de la pantalla
+
+                    canvas_left_padding = 0;
+                    canvas_top_padding =  (screen_height - (video_proportion*video_height))/2;
+                    $('.drawing-layer').attr('width', screen_width);
+                    $('.drawing-layer').attr('height', video_proportion*video_height);
+                }
+            } else {
+                // If we are not in fullscreen mode, only reset the size of the canvas
+                video_proportion = 1;
+                canvas_left_padding = 0;
+                canvas_top_padding = 0;
+                $('.drawing-layer').attr('height', $('#video-player').height());
+                $('.drawing-layer').attr('width', $('#video-player').width());
+
+            }
             $('.drawing-layer').offset($('#video-player').offset());
-            $('.drawing-layer').attr('height', $('#video-player').height());
-            $('.drawing-layer').attr('width', $('#video-player').width());
+            $('.drawing-layer').css('padding-left' , canvas_left_padding);
+            $('.drawing-layer').css('padding-top' , canvas_top_padding);
+
         });
+        video_fps = get_video_fps(video)
     }
+
+    /* Video canvas adjust size and START */
+    var video = document.getElementById("video-player");
+    // Cuando el vídeo ha cargado su tamano establecemos el
+    // mismo tamaño para el canvas
+    video.onloadedmetadata = adjustCanvas(video)
+    // Necesario porque el evento onloadedmetadata no siempre funciona
+    $(video).ready(adjustCanvas(video));
 
     /* Entering Exiting fullscreen mode */
     $('#video-player').bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
-        var fullscreenOn = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+        fullscreenOn = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
         if (fullscreenOn){
             $('.drawing-layer').css('z-index', 2147483647)
+
         }else{
             $('.drawing-layer').css('z-index', "")
         }
