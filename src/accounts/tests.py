61e66dbad0cb23135e65cb97513e7ee7c549ee3b
Iago Suárez <xoan.iago.suarez@udc.es>
2015-06-08 17:20:38 +0200

    INESTABLE! Subidos nuevos test funcionales, que fallan porque se necesita <a href="https://github.com/django/django/commit/8c99b7920e8187f98cf4d7dbd9918bd6c6da1238">una característica añadida en Django 1.8</a>

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve, reverse
+from django.core.urlresolvers import resolve
 from accounts.views import HomeView
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.urlresolvers import reverse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PageOpenTestCase(TestCase):
@@ -19,3 +26,49 @@ class PageOpenTestCase(TestCase):
         url = reverse('about')
         r = self.client.get(url)
         self.assertEqual(r.status_code, 200)
+
+
+class AccountsSeleniumTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = webdriver.Firefox()
+        cls.john = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        cls.john.save()
+        super(AccountsSeleniumTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        cls.john.delete()
+        super(AccountsSeleniumTest, cls).tearDownClass()
+
+    def test_home(self):
+        self.selenium.get(self.live_server_url)
+        title = self.selenium.find_element_by_tag_name("h1").text
+        self.assertEqual(title, "Ancoweb")
+
+    def test_login_logout(self):
+        john = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        john.save()
+        self.login_user(john, 'johnpassword')
+        self.logout_user(john)
+
+    def login_user(self, user, password):
+        self.selenium.get(self.live_server_url)
+        self.selenium.find_element_by_id('id_username').send_keys(user.username)
+        self.selenium.find_element_by_id('id_password').send_keys(password)
+        self.selenium.find_element_by_id('submit-id-sign_in').click()
+        wait = WebDriverWait(self.selenium, 1)
+        menu = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.dropdown-toggle')))
+        self.assertEqual(user.username, menu.text)
+
+    def logout_user(self, user):
+        self.selenium.find_element_by_css_selector('a.dropdown-toggle').click()
+        self.selenium.find_element_by_css_selector(
+            'ul.dropdown-menu a[href*="/accounts/logout"]').click()
+
+        wait = WebDriverWait(self.selenium, 10)
+        wait .until(EC.presence_of_element_located((By.ID, 'submit-id-sign_in')))
+        msg = self.selenium.find_element_by_css_selector('div.alert-info').text
+        assert 'Logout successful!' in msg
+        user.delete()
--- src/video_manager/tests.py
+++ src/video_manager/tests.py
@@ -1,8 +1,15 @@
+import os
 import unittest
 from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.urlresolvers import reverse
 from django.test import Client
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from ancoweb import settings
 from video_manager.models import VideoModel
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class IndexTest(unittest.TestCase):
@@ -34,7 +41,85 @@ class IndexTest(unittest.TestCase):
         self.assertEqual(r.context['object_list'][0], self.v2)
         self.assertEqual(r.context['object_list'][1], self.v1)
 
-    def addCleanup(self, function, *args, **kwargs):
+    def tearDown(self):
         self.v1.delete()
         self.v2.delete()
         self.john.delete()
+
+
+class VideoManagerSeleniumTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = webdriver.Firefox()
+        super(VideoManagerSeleniumTest, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(VideoManagerSeleniumTest, cls).tearDownClass()
+
+    def test_video_in_list(self):
+        john = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        john.save()
+        v1 = VideoModel.objects.create(title="Primero", video="tests_resources/v296.mpg",
+                                       detected_objs="tests_resources/wk1gt.xml",
+                                       description="Descripcion.", owner=john)
+        v1.save()
+        self.selenium.get(self.live_server_url + reverse('videos:index'))
+        title = self.selenium.find_element_by_css_selector(
+            "li.video-fragment div.col-xs-9 a").text
+        self.assertEqual(title, v1.title)
+
+        v1.delete()
+        john.delete()
+
+    def test_upload_video(self):
+
+        john = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        john.save()
+        # Login
+        self.login_user(john, 'johnpassword')
+
+        # Upload Video
+        self.selenium.get(self.live_server_url)
+        upload_url = reverse('video_upload:upload')
+        self.selenium.find_element_by_css_selector('a.dropdown-toggle').click()
+        self.selenium.find_element_by_css_selector(
+            'a[href*="' + upload_url + '"]').click()
+        self.selenium.find_element_by_id('id_title').send_keys("Funcional Tests")
+        self.selenium.find_element_by_id('id_video').send_keys(
+            os.path.join(str(settings.BASE_DIR),
+                         str(settings.MEDIA_ROOT),
+                         "tests_resources/wk1gt.xml"))
+        self.selenium.find_element_by_id('id_description') \
+            .send_keys("Funcional Tests Description \n this is a quite "
+                       "important test because it'll test the video uploads")
+
+        self.selenium.find_element_by_id('form_submit_button').click()
+
+        wait = WebDriverWait(self.selenium, 100)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[class*="video-finished-btn"')))
+
+        # Logout
+        self.logout_user(self.john)
+        john.delete()
+
+    def login_user(self, user, password):
+        self.selenium.get(self.live_server_url)
+        self.selenium.find_element_by_id('id_username').send_keys(user.username)
+        self.selenium.find_element_by_id('id_password').send_keys(password)
+        self.selenium.find_element_by_id('submit-id-sign_in').click()
+        wait = WebDriverWait(self.selenium, 1)
+        menu = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.dropdown-toggle')))
+        self.assertEqual(user.username, menu.text)
+
+    def logout_user(self, user):
+        self.selenium.find_element_by_css_selector('a.dropdown-toggle').click()
+        self.selenium.find_element_by_css_selector(
+            'ul.dropdown-menu a[href*="/accounts/logout"]').click()
+
+        wait = WebDriverWait(self.selenium, 10)
+        wait .until(EC.presence_of_element_located((By.ID, 'submit-id-sign_in')))
+        msg = self.selenium.find_element_by_css_selector('div.alert-info').text
+        assert 'Logout successful!' in msg
+        user.delete()
